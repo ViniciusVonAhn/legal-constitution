bf2fdd0d81c4d8bc7ff625a6895ba959640a39dd
ViniciusVonAhn <viniciusvonahn@gmail.com>
2019-07-09 10:39:29 -0300

    initial

---
+++ src/main/java/br/org/fiergs/legalconstitution/LegalConstitutionApplication.java
@@ -0,0 +1,13 @@
+package br.org.fiergs.legalconstitution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalConstitutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegalConstitutionApplication.class, args);
+    }
+
+}
